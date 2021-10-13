1000a26528b313575ad3a6acf6c2bf6409a20daa
santiago federico silva jacquier <sfsilvajacquier@gmail.com>
2021-10-12 21:19:30 -0300

    predict

--- MooDy/predict.py
+++ MooDy/predict.py
@@ -1,5 +1,5 @@
 """train - prediction of our model-"""""
-
+from tensorflow.keras.callbacks import EarlyStopping
 #
 def train_model(target, label='Up'):
   a = 0.1
@@ -65,3 +65,45 @@ def model_predict(df, target):
   df2['target'] = target['Label']
   df2['succeed'] = df2['target'] == df2['predicted_label']
   return df2
+
+#-----
+def init_model():
+    initial_learning_rate = 0.001
+    lr_schedule = ExponentialDecay(initial_learning_rate, decay_steps=2000, decay_rate=0.5)
+    adam = Adam(learning_rate=lr_schedule)
+    model = models.Sequential()
+    model.add(layers.Masking(mask_value=-1000., input_shape=(30, 1)))
+    model.add(layers.GRU(512, return_sequences=True, activation='tanh'))
+    model.add(layers.GRU(256, return_sequences=True, activation='tanh'))
+    model.add(layers.GRU(128, return_sequences=True, activation='tanh'))
+    model.add(layers.GRU(256, return_sequences=True, activation='tanh'))
+    model.add(layers.GRU(128, return_sequences=True, activation='tanh',dropout=0.1))
+    model.add(layers.GRU(64, return_sequences=True, activation='tanh',dropout=0.2))
+    model.add(layers.GRU(32, return_sequences=True, activation='tanh', dropout=0.3 ))
+    #model.add(Attention(32))
+    #model.add(Flatten())  
+    model.add(layers.Dense(3, activation='softmax'))
+    
+    model.compile(
+        optimizer=adam,
+        loss="categorical_crossentropy",
+        metrics=["categorical_accuracy"],
+    )
+    return model
+
+init_model().summary()
+
+#-------
+
+
+model = init_model()
+
+es = EarlyStopping(monitor='val_loss', verbose=1, patience=10, restore_best_weights=True)
+
+history = model.fit(X_train_pad, y_train_pad,
+           epochs=1000,
+           validation_split=0.2, 
+           batch_size=32,
+           callbacks=[es], 
+           verbose=1,
+           )
