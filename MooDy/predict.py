000b0b2561d97694259a97ea0ea28a200b316db6
santiago federico silva jacquier <sfsilvajacquier@gmail.com>
2021-10-12 22:01:50 -0300

    deploy

--- MooDy/predict.py
+++ MooDy/predict.py
@@ -1,71 +1,21 @@
 """train - prediction of our model-"""""
+import numpy as np
 from tensorflow.keras.callbacks import EarlyStopping
-#
-def train_model(target, label='Up'):
-  a = 0.1
-  b = 0.1
-  c = 0.1
-  d = 0.1
-  e = 0.1
-  f = 0.1
-  g = 0.1
-  h = 0.1
-  n_epoch = 200
-  loss_history = [loss(target[label],df_blue,a,b,c,d,e,f,g,h)]
-  a_history = [a]
-  b_history = [b]
-  c_history = [c]
-  d_history = [d]
-  e_history = [e]
-  f_history = [f]
-  g_history = [g]
-  h_history = [h]
-
-  for epoch in range(n_epoch):
-      new_a = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[0]
-      new_b = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[1]
-      new_c = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[2]
-      new_d = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[3]
-      new_e = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[4]
-      new_f = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[5]
-      new_g = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[6]
-      new_h = update_params(a,b,c,d,e,f,g,h, *steps(*gradient(target[label],df_blue,a,b,c,d,e,f,g,h), learning_rate=0.003))[7]
-      a = new_a
-      b = new_b
-      c = new_c
-      d = new_d
-      e = new_e
-      f = new_f
-      g = new_g
-      h = new_h
-      loss_history.append(loss(target[label],df_blue,a,b,c,d,e,f,g,h))
-      a_history.append(a)
-      b_history.append(b)
-      c_history.append(c)
-      d_history.append(d)
-      e_history.append(e)
-      f_history.append(f)
-      g_history.append(g)
-      h_history.append(h)
-  print(loss(target[label],df_blue,a,b,c,d,e,f,g,h))
-  print(a,b,c,d,e,f,g,h)
-  return a,b,c,d,e,f,g,h
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from keras.layers import Flatten
+from tensorflow.keras.optimizers import RMSprop, Adam, SGD
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from model import get_X_y
+from sklearn.model_selection import train_test_split
+from index import tweet_index
+from datos import get_data, get_clean_data
 
 
-def model_predict(df, target):
-  labels = ['Label']
-  df2 = pd.DataFrame()
-  for lab in labels:
-    a, b, c, d , e, f, g, h= train_model(target, label=lab)
-    best_label = tweet_index(df, a, b, c, d, e, f, g, h)
-    best_label[lab] = best_label.indice.map(lambda x : sigmoid(x))
-    df2 = df2.join(best_label[lab], how='outer')
-  df2['predicted_label'] = df2[['lab']]
-  df2['predicted_label'] = df2['predicted_label'].astype(int)
-  df2['target'] = target['Label']
-  df2['succeed'] = df2['target'] == df2['predicted_label']
-  return df2
-
+df1, df2 = get_data()   
+df_blue = get_clean_data(df1,df2)
 #-----
 def init_model():
     initial_learning_rate = 0.001
@@ -91,19 +41,35 @@ def init_model():
     )
     return model
 
-init_model().summary()
+#-------
+def train_split(): 
+    # Here we define the parameter to generate our train/test sets
+    train_size = 600
+    #test_size = round(0.6*train_size)
+
+    min_seq_len = 20
+    max_seq_len = 30
+
+    sequence_lengths_train = np.random.randint(low=min_seq_len, high=max_seq_len, size=train_size)
+    X, y = get_X_y(df, sequence_lengths_train)
+
+    X_train, y_train = train_test_split(X, y, test_size=0.3, random_state=42)
+    return X_train, y_train
 
 #-------
 
+def train_model():
+    X_train_pad = pad_sequences(X_train, value=-1000., dtype=float, padding='post', maxlen=30)
+    y_train_pad = pad_sequences(y_train, value=-1000., dtype=float, padding='post', maxlen=30)
+    model = init_model()
 
-model = init_model()
+    es = EarlyStopping(monitor='val_loss', verbose=1, patience=10, restore_best_weights=True)
 
-es = EarlyStopping(monitor='val_loss', verbose=1, patience=10, restore_best_weights=True)
-
-history = model.fit(X_train_pad, y_train_pad,
-           epochs=1000,
-           validation_split=0.2, 
-           batch_size=32,
-           callbacks=[es], 
-           verbose=1,
-           )
+    history = model.fit(X_train_pad, y_train_pad,
+            epochs=1000,
+            validation_split=0.2, 
+            batch_size=32,
+            callbacks=[es], 
+            verbose=1,
+            )
+    return history
